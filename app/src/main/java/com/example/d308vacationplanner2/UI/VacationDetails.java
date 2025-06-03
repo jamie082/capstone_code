3bcc97d3b7290f4189a3a98c44dfd04703946a81
Jamie Morrissey <jmo1344@wgu.edu>
2025-06-02 17:07:49 -0700

    TASK B2

--- app/src/main/java/com/example/d308vacationplanner2/UI/VacationDetails.java
+++ app/src/main/java/com/example/d308vacationplanner2/UI/VacationDetails.java
@@ -1,11 +1,14 @@
 package com.example.d308vacationplanner2.UI;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -22,8 +25,13 @@ import com.example.d308vacationplanner2.entities.Excursion;
 import com.example.d308vacationplanner2.entities.Vacation;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class VacationDetails extends AppCompatActivity {
 
@@ -36,7 +44,17 @@ public class VacationDetails extends AppCompatActivity {
 
     Vacation currentVacation;
 
+    TextView editStartVacaDate;
+    TextView editEndVacaDate;
+
     int numParts;
+
+    DatePickerDialog.OnDateSetListener startVacaDate;
+    DatePickerDialog.OnDateSetListener endVacaDate;
+    final Calendar myCalendarStart = Calendar.getInstance();
+    final Calendar myCalendarEnd = Calendar.getInstance();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +62,16 @@ public class VacationDetails extends AppCompatActivity {
         setContentView(R.layout.activity_vacation_details);
         FloatingActionButton fab = findViewById(R.id.floatingActionButton2);
         editName = findViewById(R.id.name);
-        editPrice=findViewById(R.id.price);
+        editPrice = findViewById(R.id.price);
+        editStartVacaDate = findViewById(R.id.startvacationdate);
+        editEndVacaDate = findViewById(R.id.endvacationdate);
         productID = getIntent().getIntExtra("id", -1);
         name = getIntent().getStringExtra("name");
         price = getIntent().getDoubleExtra("price", 0.0);
         editName.setText(name);
         editPrice.setText(Double.toString(price));
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,6 +89,96 @@ public class VacationDetails extends AppCompatActivity {
             if (p.getVacationID() == productID) filteredParts.add(p);
         }
         excursionAdapter.setParts(filteredParts);
+
+        startVacaDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+
+                myCalendarStart.set(Calendar.YEAR, year);
+                myCalendarStart.set(Calendar.MONTH, monthOfYear);
+                myCalendarStart.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+
+                updateLabelStart();
+            }
+
+        };
+
+        endVacaDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+
+                myCalendarEnd.set(Calendar.YEAR, year);
+                myCalendarEnd.set(Calendar.MONTH, monthOfYear);
+                myCalendarEnd.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+
+                updateLabelEnd();
+            }
+
+        };
+
+        editStartVacaDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Date date;
+                //get value from other screen,but I'm going to hard code it right now
+                String info=editStartVacaDate.getText().toString();
+                if(info.equals(""))info="02/10/24";
+                try{
+                    myCalendarStart.setTime(sdf.parse(info));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                new DatePickerDialog(VacationDetails.this, startVacaDate, myCalendarStart
+                        .get(Calendar.YEAR), myCalendarStart.get(Calendar.MONTH),
+                        myCalendarStart.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        editEndVacaDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Date date;
+                //get value from other screen,but I'm going to hard code it right now
+                String info=editEndVacaDate.getText().toString();
+                if(info.equals(""))info="02/10/24";
+                try{
+                    myCalendarEnd.setTime(sdf.parse(info));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                DatePickerDialog datePickerDialog = new DatePickerDialog(VacationDetails.this, endVacaDate, myCalendarStart
+                        .get(Calendar.YEAR), myCalendarStart.get(Calendar.MONTH),
+                        myCalendarStart.get(Calendar.DAY_OF_MONTH));
+
+                datePickerDialog.getDatePicker().setMinDate(myCalendarStart.getTimeInMillis());
+
+                datePickerDialog.show();
+            }
+        });
+    }
+    private void updateLabelStart() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        editStartVacaDate.setText(sdf.format(myCalendarStart.getTime()));
+    }
+    private void updateLabelEnd() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        editEndVacaDate.setText(sdf.format(myCalendarEnd.getTime()));
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -75,35 +187,34 @@ public class VacationDetails extends AppCompatActivity {
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.productsave) {
+        if (item.getItemId() == R.id.productsave) {
             Vacation vacation;
-            if(productID==-1) {
+            if (productID == -1) {
                 if (repository.getmAllVacations().size() == 0) productID = 1;
-                else productID = repository.getmAllVacations().get(repository.getmAllVacations().size() - 1).getVacationID() + 1;
+                else
+                    productID = repository.getmAllVacations().get(repository.getmAllVacations().size() - 1).getVacationID() + 1;
                 vacation = new Vacation(productID, editName.getText().toString(), Double.parseDouble(editPrice.getText().toString()));
                 repository.insert(vacation);
                 this.finish();
-            }
-            else {
+            } else {
                 vacation = new Vacation(productID, editName.getText().toString(), Double.parseDouble(editPrice.getText().toString()));
                 repository.update(vacation);
                 this.finish();
             }
         }
-        if(item.getItemId()==R.id.productdelete) {
-            for(Vacation prod:repository.getmAllVacations()) {
-                if(prod.getVacationID()==productID)currentVacation=prod;
+        if (item.getItemId() == R.id.productdelete) {
+            for (Vacation prod : repository.getmAllVacations()) {
+                if (prod.getVacationID() == productID) currentVacation = prod;
             }
-            numParts=0;
-            for(Excursion part: repository.getAllParts()) {
-                if(part.getVacationID()==productID)++numParts;
+            numParts = 0;
+            for (Excursion part : repository.getAllParts()) {
+                if (part.getVacationID() == productID) ++numParts;
             }
             if (numParts == 0) {
                 repository.delete(currentVacation);
                 Toast.makeText(VacationDetails.this, currentVacation.getVacationName() + " was deleted", Toast.LENGTH_LONG).show();
                 VacationDetails.this.finish();
-            }
-            else {
+            } else {
                 Toast.makeText(VacationDetails.this, "Can't delete a product with parts", Toast.LENGTH_LONG).show();
 
             }
